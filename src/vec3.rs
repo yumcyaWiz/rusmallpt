2908c8019604c5f5adf5178e08a75375868cab2e
yumcyawiz <hashdolf1923@gmail.com>
2022-03-07 03:28:30 +0900

    add pub

--- src/vec3.rs
+++ src/vec3.rs
@@ -8,26 +8,26 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
-  fn new(x: Real, y: Real, z: Real) -> Self {
+  pub fn new(x: Real, y: Real, z: Real) -> Self {
     Vec3 {
       elements: [x, y, z],
     }
   }
 
-  fn x(&self) -> Real {
+  pub fn x(&self) -> Real {
     self.elements[0]
   }
 
-  fn y(&self) -> Real {
+  pub fn y(&self) -> Real {
     self.elements[1]
   }
 
-  fn z(&self) -> Real {
+  pub fn z(&self) -> Real {
     self.elements[2]
   }
 }
 
-fn dot(v1: &Vec3, v2: &Vec3) -> Real {
+pub fn dot(v1: &Vec3, v2: &Vec3) -> Real {
   let mut sum: Real = 0.0;
   for (k, _) in v1.elements.iter().enumerate() {
     sum += v1.elements[k] * v2.elements[k];
@@ -35,7 +35,7 @@ fn dot(v1: &Vec3, v2: &Vec3) -> Real {
   sum
 }
 
-fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
+pub fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
   Vec3 {
     elements: [
       v1.y() * v2.z() - v1.z() * v2.y(),
@@ -45,11 +45,11 @@ fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
   }
 }
 
-fn length(v: &Vec3) -> Real {
+pub fn length(v: &Vec3) -> Real {
   dot(v, v).sqrt()
 }
 
-fn length2(v: &Vec3) -> Real {
+pub fn length2(v: &Vec3) -> Real {
   dot(v, v)
 }
 
