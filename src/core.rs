d7c97bc7f1e1802b1e038e21a77849f2beedb018
yumcyawiz <hashdolf1923@gmail.com>
2022-03-07 03:25:51 +0900

    impl ray position func

--- src/core.rs
+++ src/core.rs
@@ -1,19 +1,41 @@
 use crate::types::Real;
 use crate::vec3::Vec3;
 
-struct Ray {
+pub struct Ray {
   origin: Vec3,
   direction: Vec3,
+  tmin: Real,
+  tmax: Real,
 }
 
 impl Ray {
-  fn position(&self, t: Real) -> Vec3 {
+  pub fn new(origin: Vec3, direction: Vec3) -> Self {
+    Ray {
+      origin,
+      direction,
+      tmin: 1E-3,
+      tmax: 1E9,
+    }
+  }
+
+  pub fn position(&self, t: Real) -> Vec3 {
     &self.origin + t * &self.direction
   }
 }
 
-struct IntersectInfo {
+pub struct IntersectInfo {
   t: Real,
   hit_pos: Vec3,
   hit_normal: Vec3,
+}
+
+#[cfg(test)]
+mod test {
+  use crate::core::*;
+
+  #[test]
+  fn ray_position() {
+    let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0));
+    assert_eq!(ray.position(1.0), Vec3::new(0.0, 0.0, 1.0));
+  }
 }
