429e32d6b856c5055ce98f78e8946ec1dbbca2d6
yumcyawiz <hashdolf1923@gmail.com>
2022-03-07 20:49:42 +0900

    add sample_ray unit test

--- src/camera.rs
+++ src/camera.rs
@@ -21,10 +21,10 @@ impl Camera {
     }
   }
 
-  pub fn sample_ray(&self, uv: &Vec2) {
+  pub fn sample_ray(&self, uv: &Vec2) -> Ray {
     let sensor_pos = &(&self.position + uv.x() * &self.right + uv.y() * &self.up);
     let pinhole_pos = &(&self.position + &self.forward);
-    Ray::new(sensor_pos, &(pinhole_pos - sensor_pos).normalize());
+    Ray::new(sensor_pos, &(pinhole_pos - sensor_pos).normalize())
   }
 }
 
@@ -39,5 +39,17 @@ mod tests {
     assert_eq!(camera.forward, Vec3::new(0.0, 0.0, -1.0));
     assert_eq!(camera.right, Vec3::new(1.0, 0.0, 0.0));
     assert_eq!(camera.up, Vec3::new(0.0, 1.0, 0.0));
+  }
+
+  #[test]
+  fn sample_ray() {
+    let camera = Camera::new(&Vec3::new(0.0, 0.0, 0.0), &Vec3::new(0.0, 0.0, -1.0));
+    let sensor_pos = &Vec3::new(1.0, 1.0, 0.0);
+    let pinhole_pos = &Vec3::new(0.0, 0.0, -1.0);
+
+    assert_eq!(
+      camera.sample_ray(&Vec2::new(1.0, 1.0)),
+      Ray::new(sensor_pos, &(pinhole_pos - sensor_pos).normalize())
+    );
   }
 }
