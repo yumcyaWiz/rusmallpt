23775a64c35e7863665d8e46d6b4d98c7da36c8c
yumcyawiz <hashdolf1923@gmail.com>
2022-03-09 02:03:04 +0900

    test linear intersector

--- src/intersector.rs
+++ src/intersector.rs
@@ -1,17 +1,23 @@
 use crate::core::{IntersectInfo, Intersectable, Ray};
 
-struct Intersector {
+pub struct Intersector {
     intersectables: Vec<Box<dyn Intersectable>>,
+}
+
+impl Intersector {
+    pub fn new(intersectables: Vec<Box<dyn Intersectable>>) -> Self {
+        Intersector { intersectables }
+    }
 }
 
 impl Intersectable for Intersector {
     fn intersect(&self, ray: &Ray) -> Option<IntersectInfo> {
-        let mut t = ray.tmin;
+        let mut t = ray.tmax;
         let mut info: Option<IntersectInfo> = None;
         for intersectable in &self.intersectables {
             match intersectable.intersect(ray) {
                 Some(info_each) => {
-                    if t < info_each.t {
+                    if info_each.t < t {
                         t = info_each.t;
                         info = Some(info_each);
                     }
