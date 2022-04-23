4524f00521fa564fdf881e327e106951ca049bdb
yumcyawiz <hashdolf1923@gmail.com>
2022-04-23 17:24:25 +0900

    add plane shape

--- src/shape.rs
+++ src/shape.rs
@@ -39,6 +39,58 @@ impl IntersectableLocal for Sphere {
     }
 }
 
+pub struct Plane {
+    left_corner_point: Vec3,
+    right: Vec3,
+    up: Vec3,
+
+    center: Vec3,
+    normal: Vec3,
+    right_dir: Vec3,
+    up_dir: Vec3,
+
+    right_dir_length: Real,
+    up_dir_length: Real,
+}
+
+impl Plane {
+    pub fn new(left_corner_point: Vec3, right: Vec3, up: Vec3) -> Self {
+        Plane {
+            left_corner_point,
+            right,
+            up,
+            center: left_corner_point + 0.5 * right + 0.5 * up,
+            normal: right.cross(up).normalize(),
+            right_dir: right.normalize(),
+            up_dir: up.normalize(),
+            right_dir_length: right.length(),
+            up_dir_length: up.length(),
+        }
+    }
+}
+
+impl IntersectableLocal for Plane {
+    fn intersect(&self, ray: &Ray) -> Option<IntersectInfoLocal> {
+        let t = -(ray.origin - self.center).dot(self.normal) / ray.direction.dot(self.normal);
+        if t < ray.tmin || t > ray.tmax {
+            return None;
+        }
+
+        let pos = ray.position(t);
+        let dx = (pos - self.left_corner_point).dot(self.right_dir);
+        let dy = (pos - self.left_corner_point).dot(self.up_dir);
+        if dx < 0.0 || dx > self.right_dir_length || dy < 0.0 || dy > self.up_dir_length {
+            return None;
+        }
+
+        Some(IntersectInfoLocal {
+            t,
+            pos,
+            normal: self.normal,
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::shape::*;
@@ -55,5 +107,23 @@ mod tests {
                 normal: Vec3::new(0.0, 0.0, -1.0)
             })
         );
+    }
+
+    #[test]
+    fn plane_intersect() {
+        let plane = Plane::new(
+            Vec3::new(-1.0, 0.0, 1.0),
+            Vec3::new(2.0, 0.0, 0.0),
+            Vec3::new(0.0, 0.0, -2.0),
+        );
+        let ray = Ray::new(Vec3::new(0.0, 1.0, 0.0), Vec3::new(0.0, -1.0, 0.0));
+        assert_eq!(
+            plane.intersect(&ray),
+            Some(IntersectInfoLocal {
+                t: 1.0,
+                pos: Vec3::new(0.0, 0.0, 0.0),
+                normal: Vec3::new(0.0, 1.0, 0.0)
+            })
+        )
     }
 }
