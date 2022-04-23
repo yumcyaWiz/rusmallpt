7490a5da18bd30c5ee993fd8c95e1de887ffebce
yumcyawiz <hashdolf1923@gmail.com>
2022-04-23 14:58:24 +0900

    refactor bxdf interface, add IdealReflection bxdf

--- src/bxdf.rs
+++ src/bxdf.rs
@@ -1,11 +1,28 @@
 use std::f32::consts::FRAC_1_PI;
 
+use crate::core::ShadingInfo;
 use crate::sampler::{cosine_weighted_hemisphere, Sampler};
 use crate::types::Real;
 use crate::vec3::Vec3;
 
+// NOTE: assuming vectors in tangent space
+
+fn reflect(v: Vec3, n: Vec3) -> Vec3 {
+    -v + 2.0 * v.dot(n) * n
+}
+
+fn absCosTheta(v: Vec3) -> Real {
+    v.y().abs()
+}
+
+pub struct BxDFSample {
+    pub f: Vec3,   // BxDF value
+    pub wi: Vec3,  // sampled direction
+    pub pdf: Real, // pdf
+}
+
 pub trait BxDF {
-    fn sample_bxdf(&self, sampler: &mut Sampler) -> (Vec3, Vec3, Real);
+    fn sample_direction(&self, info: &ShadingInfo, sampler: &mut Sampler) -> BxDFSample;
 }
 
 pub struct Lambert {
@@ -19,9 +36,26 @@ impl Lambert {
 }
 
 impl BxDF for Lambert {
-    fn sample_bxdf(&self, sampler: &mut Sampler) -> (Vec3, Vec3, Real) {
+    fn sample_direction(&self, info: &ShadingInfo, sampler: &mut Sampler) -> BxDFSample {
         let uv = sampler.next_2d();
-        let (direction, pdf) = cosine_weighted_hemisphere(uv);
-        (FRAC_1_PI * self.rho, direction, pdf)
+        let (wi, pdf) = cosine_weighted_hemisphere(uv);
+        BxDFSample {
+            f: FRAC_1_PI * self.rho,
+            wi,
+            pdf,
+        }
+    }
+}
+
+pub struct IdealReflection {}
+
+impl BxDF for IdealReflection {
+    fn sample_direction(&self, info: &ShadingInfo, sampler: &mut Sampler) -> BxDFSample {
+        let wi = reflect(info.wo, info.n);
+        BxDFSample {
+            f: Vec3::new(1.0, 1.0, 1.0) / absCosTheta(wi),
+            wi,
+            pdf: 1.0,
+        }
     }
 }
